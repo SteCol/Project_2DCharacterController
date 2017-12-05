65d9c3b7a02a2128158401c1c103ad62a496eb06
Stef <stef.col@hotmail.com>
2017-12-06 00:39:14 +0100

    added unity project

---
+++ Unity_2DCharacterController/Assets/scr_PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        Movement();
+	}
+
+    void Movement()
+    {
+        this.GetComponent<Rigidbody2D>().AddForce((new Vector2(Input.GetAxis("Horizontal"), 0)), ForceMode2D.Impulse);
+        
+        //this.transform.position = Input.GetAxis("Horizontal");
+    }
+}
